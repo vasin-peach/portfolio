80f7de2ad163515d3240bc1967873d260d281e73
Peaches <shinasaki07@gmail.com>
2020-03-13 18:12:25 +0700

    :art:|:heavy_plus_sign: init gsap, magic scroll.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import VueScrollmagic from "vue-scrollmagic";
 
+Vue.use(VueScrollmagic);
 Vue.config.productionTip = false;
 
 new Vue({
